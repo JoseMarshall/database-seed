d4d7fa627930f9debacb1ca8994e6f49ee8de79e
JoseMarshall <hebojosemar@gmail.com>
2021-10-06 00:04:07 +0100

    feat: client seed

---
+++ src/seeders/client-seed/index.ts
@@ -0,0 +1,27 @@
+import fs from 'fs';
+
+import { ClientDTO } from '../../dto';
+import { IClient } from '../../dto/dto.types';
+import uow from '../../main/external/repositories/mongodb/unit-of-work';
+import { logger } from '../../utils';
+import { ISeeder } from '../seeders.types';
+
+// eslint-disable-next-line import/prefer-default-export
+export async function clientSeed({ reader, path }: ISeeder<IClient>) {
+  const unitOfWork = await uow();
+  try {
+    const { rows } = await reader(fs.createReadStream(path), {
+      map: ClientDTO.schema,
+    });
+
+    const clients = rows.map(ClientDTO.mapper);
+
+    const repo = unitOfWork.makeClientRepository();
+
+    const result = await repo.add(clients);
+
+    logger.info(`CLIENT_SEED SUCCESS Created ${result.length} of ${rows.length}`);
+  } catch (error) {
+    logger.error(error);
+  }
+}
